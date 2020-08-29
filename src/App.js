24bdc5f2caff5112748d499554c201ee3c8ad4d0
shubham singh <shubham.singh2882@gmail.com>
2020-08-29 23:41:10 +0530

    learn about react component use and build a list and insert into the list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+// class HelloMessage extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         Hello {this.props.name}
+//       </div>
+//     );
+//   }
+// }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+class TaskList extends React.Component {
+  state = {
+    items: [
+      { id: 1, name: "Dubug service", isDone: false },
+      { id: 2, name: "Buy chocolates", isDone: false },
+      { id: 3, name: "Watch anime ", isDone: false },
+      { id: 4, name: "Pubg ", isDone: false }
+    ]
+  };
+  render() {
+    const { items } = this.state;
+
+    return (
+      <div>
+        <button
+          onClick={() => {
+            const name = prompt("Enter Task");
+            if (name) {
+              this.setState(state => ({
+                items: [...state.items, { id: state.items.length, name: name, isDone: false}]
+              }));
+            }
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Add Item
+        </button>
+        <ul>
+          {items.map(({ id, name }) => (
+            <li key={id}>{name}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default App;
+// export default App;
+export default TaskList;
